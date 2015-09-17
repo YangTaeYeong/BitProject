aaeb5422c3227dd828e01ebc6f83da17c5bcfa49
양태영 <poongbreak@naver.com>
2015-09-17 22:02:43 +0900

    fdfdd

--- Prototype1.4/Prototype/app/src/main/java/com/example/administrator/myapplication/RecordingResultActivity.java
+++ Prototype1.4/Prototype/app/src/main/java/com/example/administrator/myapplication/RecordingResultActivity.java
@@ -5,17 +5,28 @@ import java.util.List;
 import java.util.Random;
 
 import android.app.Activity;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
+import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
 
-public class RecordingResultActivity extends Activity {
-
+public class RecordingResultActivity extends Activity implements View.OnTouchListener {
+    private static final int SERIES_SIZE = 200;
+    private XYPlot mySimpleXYPlot;
+    private Button resetButton;
+    private SimpleXYSeries[] series = null;
+    private PointF minXY;
+    private PointF maxXY;
     private XYPlot plot;
 
     @Override
@@ -24,13 +35,13 @@ public class RecordingResultActivity extends Activity {
         setContentView(R.layout.xy_plot_chart);
         plot = (XYPlot) findViewById(R.id.xyPlot);
 
-        List s1 = getSeries(20, 10);
+        List s1 = getSeries(10, 10);
         XYSeries series1 = new SimpleXYSeries(s1,
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series 1");
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "내 목소리");
 
-        List s2 = getSeries(20, 10);
+        List s2 = getSeries(10, 10);
         XYSeries series2 = new SimpleXYSeries(s2,
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series 2");
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "원곡");
 
         LineAndPointFormatter s1Format = new LineAndPointFormatter();
         s1Format.setPointLabelFormatter(new PointLabelFormatter());
@@ -63,5 +74,109 @@ public class RecordingResultActivity extends Activity {
             series.add(rand.nextInt(max));
         }
         return series;
+    }
+
+    private void populateSeries(SimpleXYSeries series, int max) {
+        Random r = new Random();
+        for(int i = 0; i < SERIES_SIZE; i++) {
+            series.addLast(i, r.nextInt(max));
+        }
+    }
+
+    // Definition of the touch states
+    static final int NONE = 0;
+    static final int ONE_FINGER_DRAG = 1;
+    static final int TWO_FINGERS_DRAG = 2;
+    int mode = NONE;
+
+    PointF firstFinger;
+    float distBetweenFingers;
+    boolean stopThread = false;
+
+
+
+    private void zoom(float scale) {
+        float domainSpan = maxXY.x - minXY.x;
+        float domainMidPoint = maxXY.x - domainSpan / 2.0f;
+        float offset = domainSpan * scale / 2.0f;
+
+        minXY.x = domainMidPoint - offset;
+        maxXY.x = domainMidPoint + offset;
+
+        minXY.x = Math.min(minXY.x, series[3].getX(series[3].size() - 3)
+                .floatValue());
+        maxXY.x = Math.max(maxXY.x, series[0].getX(1).floatValue());
+        clampToDomainBounds(domainSpan);
+    }
+
+    private void scroll(float pan) {
+        float domainSpan = maxXY.x - minXY.x;
+        float step = domainSpan / mySimpleXYPlot.getWidth();
+        float offset = pan * step;
+        minXY.x = minXY.x + offset;
+        maxXY.x = maxXY.x + offset;
+        clampToDomainBounds(domainSpan);
+    }
+
+    private void clampToDomainBounds(float domainSpan) {
+        float leftBoundary = series[0].getX(0).floatValue();
+        float rightBoundary = series[3].getX(series[3].size() - 1).floatValue();
+        // enforce left scroll boundary:
+        if (minXY.x < leftBoundary) {
+            minXY.x = leftBoundary;
+            maxXY.x = leftBoundary + domainSpan;
+        } else if (maxXY.x > series[3].getX(series[3].size() - 1).floatValue()) {
+            maxXY.x = rightBoundary;
+            minXY.x = rightBoundary - domainSpan;
+        }
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: // Start gesture
+                firstFinger = new PointF(event.getX(), event.getY());
+                mode = ONE_FINGER_DRAG;
+                stopThread = true;
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN: // second finger
+                distBetweenFingers = spacing(event);
+                // the distance check is done to avoid false alarms
+                if (distBetweenFingers > 5f) {
+                    mode = TWO_FINGERS_DRAG;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == ONE_FINGER_DRAG) {
+                    PointF oldFirstFinger = firstFinger;
+                    firstFinger = new PointF(event.getX(), event.getY());
+                    scroll(oldFirstFinger.x - firstFinger.x);
+                    mySimpleXYPlot.setDomainBoundaries(minXY.x, maxXY.x,
+                            BoundaryMode.FIXED);
+                    mySimpleXYPlot.redraw();
+
+                } else if (mode == TWO_FINGERS_DRAG) {
+                    float oldDist = distBetweenFingers;
+                    distBetweenFingers = spacing(event);
+                    zoom(oldDist / distBetweenFingers);
+                    mySimpleXYPlot.setDomainBoundaries(minXY.x, maxXY.x,
+                            BoundaryMode.FIXED);
+                    mySimpleXYPlot.redraw();
+                }
+                break;
+        }
+        return true;
+
     }
 }
